538d3341686b0d90771fbe22929eda37663c8491
nntto <nattou-king@hotmail.co.jp>
2022-04-16 22:43:28 +0900

    'conoha-cliが動かなかったので、ローカルに移して手直しした'

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
-from os import getenv
+from os import environ
 import traceback
+from dotenv import load_dotenv
+load_dotenv()
 
 from conoha.config import Config
 from conoha.api import Token
@@ -8,10 +10,23 @@ from conoha.compute import VMList
 
 configDict = {
 	'api': {
-		'user':   getenv('CONOHA_USERNAME'),
-		'passwd': getenv('CONOHA_PASSWORD'),
-		'tenant': getenv('CONOHA_TENANT_ID'),
-	}
+		'user':   environ['CONOHA_USERNAME'],
+		'passwd': environ['CONOHA_PASSWORD'],
+		'tenant': environ['CONOHA_TENANT'],
+	},
+    'endpoint': {
+        'region': environ['CONOHA_REGION'],
+        'account': 'https://account.{REGION}.conoha.io/v1/{TENANT_ID}',
+        'compute': 'https://compute.{REGION}.conoha.io/v2/{TENANT_ID}',
+        'volume': 'https://block-storage.{REGION}.conoha.io/v2/{TENANT_ID}',
+        'database': 'https://database-hosting.{REGION}.conoha.io/v1',
+        'image': 'https://image-service.{REGION}.conoha.io/v2',
+        'dns': 'https://dns-service.{REGION}.conoha.io/v1',
+        'object': 'https://object-storage.{REGION}.conoha.io/v1/nc_{TENANT_ID}',
+        'mail': 'https://mail-hosting.{REGION}.conoha.io/v1',
+        'identity': 'https://identity.{REGION}.conoha.io/v2.0',
+        'network': 'https://networking.{REGION}.conoha.io/v2.0',
+    },
 }
 conf = Config(fromDict=configDict)
 token = Token(conf)
@@ -30,5 +45,5 @@ async def ping(ctx):
     await ctx.send('pong')
 
 
-token = getenv('DISCORD_BOT_TOKEN')
+token = environ['DISCORD_BOT_TOKEN']
 bot.run(token)
