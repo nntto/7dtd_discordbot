1a460a17431ecf89997621be0cf910cac15af6bb
nntto <nattou-king@hotmail.co.jp>
2022-04-17 01:56:33 +0900

    'サーバー管理コマンド追加'

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,10 @@
 from discord.ext import commands
+import discord
 from os import environ
 import traceback
+import time
+from dotenv import load_dotenv
+load_dotenv()
 
 from conoha.config import Config
 from conoha.api import Token
@@ -27,21 +31,91 @@ configDict = {
     },
 }
 conf = Config(fromDict=configDict)
-token = Token(conf)
+conoha_token = Token(conf)
+
+vm = None
+for i in VMList(conoha_token):
+    if i.name == environ['VM_GAMESERVER_NAME']:
+        vm = i
 
 bot = commands.Bot(command_prefix='/')
+
+@bot.event
+async def on_ready():
+    # ゲームサーバーのVMが見つからなかった時、エラーメッセージを送信
+    channel = bot.get_channel(int(environ['DISCORD_CHANNEL_ID']))
+    if vm == None:
+        await channel.send('VMサーバーが見つかりませんでした')
+    else:
+        await channel.send('おはよう！')
+        await channel.send('/help')
 
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+
     await ctx.send(error_msg)
 
+@bot.command()
+async def start(ctx):
+    """サーバーを起動"""
+    if vm.getStatus() == "ACTIVE":
+        await ctx.send("起動済みです")
+    else:
+        vm.start()
+        await ctx.send("起動中")
+        for i in range(60):
+            time.sleep(1)
+            print(vm.getStatus())
+            if vm.getStatus() == "ACTIVE":
+                await ctx.send("起動完了")
+                return 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def stop(ctx):
+    """サーバーを停止"""
+    if vm.getStatus() == "SHUTOFF":
+        await ctx.send("停止済みです")
+    else:
+        vm.stop()
+        await ctx.send("停止中")
+        for i in range(60):
+            time.sleep(1)
+            print(vm.getStatus())
+            if vm.getStatus() == "SHUTOFF":
+                await ctx.send("停止完了")
+                return 
 
+@bot.command()
+async def restart(ctx):
+    """サーバーを再起動"""
+    if vm.getStatus() == "SHUTOFF":
+        await ctx.send("起動します")
+        await start(ctx)
+    elif vm.getStatus() == "REBOOT":
+        await ctx.send("再起動中")
+    else:
+        vm.restart()
+        await ctx.send("再起動中")
+        for i in range(60):
+            time.sleep(1)
+            print(vm.getStatus())
+            if vm.getStatus() == "ACTIVE":
+                await ctx.send("再起動完了")
+                return 
+@bot.command(name="info")
+async def info(ctx):
+    """サーバーの情報を確認"""
+    embed = discord.Embed(title="サーバー情報", description="ゲームサーバーと管理画面のログイン情報", color=0xeee657)
 
-token = environ['DISCORD_BOT_TOKEN']
-bot.run(token)
+    embed.add_field(name="IPアドレス", value=str(vm.addressList[0].addr))
+    embed.add_field(name="ポート", value=f"26900")
+    embed.add_field(name="サーバーの状態", value=vm.getStatus())
+    embed.add_field(name="管理画面", value="http://163.44.252.170:8080/")
+    embed.add_field(name="管理画面パス", value="pNw6KB6VX1DoTGH1")
+
+    await ctx.send(embed=embed)
+
+discord_token = environ['DISCORD_BOT_TOKEN']
+bot.run(discord_token)
